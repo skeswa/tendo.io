257b9c88da31cafa4e9b8eec4a2719832c56f738
yasabere <yasabere@gmail.com>
2013-11-10 02:37:43 -0500

    finished upload rom html

--- public/js/controllers/setup.js
+++ public/js/controllers/setup.js
@@ -1,5 +1,44 @@
 angular.module("mean.system").controller("setupController", ["$scope", "Global", "TendoService",
   function($scope, Global, TendoService) {
-    
+
+    $(':file').change(function(){
+        var file = this.files[0];
+        name = file.name;
+        size = file.size;
+        type = file.type;
+        //Your validation
+    });
+
+    $(':button').click(function(){
+        var formData = new FormData($('form')[0]);
+        $.ajax({
+            url: '/roms/upload',  //Server script to process data
+            type: 'POST',
+            xhr: function() {  // Custom XMLHttpRequest
+                var myXhr = $.ajaxSettings.xhr();
+                if(myXhr.upload){ // Check if upload property exists
+                    myXhr.upload.addEventListener('progress',progressHandlingFunction, false); // For handling the progress of the upload
+                }
+                return myXhr;
+            },
+            //Ajax events
+            beforeSend: beforeSendHandler,
+            success: completeHandler,
+            error: errorHandler,
+            // Form data
+            data: formData,
+            //Options to tell jQuery not to process data or worry about content-type.
+            cache: false,
+            contentType: false,
+            processData: false
+        });
+    });
+
+    function progressHandlingFunction(e){
+        if(e.lengthComputable){
+            $('progress').attr({value:e.loaded,max:e.total});
+        }
+    }
+
   }
 ]);
