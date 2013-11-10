3bed7c4413332e12e6c2f22cab164aaa2de30b69
Sandile Keswa <sandile.keswa@gmail.com>
2013-11-10 02:36:54 -0500

    ..

--- public/js/controllers/controller.js
+++ public/js/controllers/controller.js
@@ -7,54 +7,55 @@ var makeNewPeerId = function() {
   return text;
 };
 
-angular.module("mean.system").factory('TendoService', function($rootScope, $http, $location) {
-  var sharedService = {};
+// angular.module("mean.system").factory('TendoService', function($rootScope, $http, $location) {
+//   var sharedService = {};
 
-  // Create a new Peer with our demo API key, with debug set to true so we can
-  // see what's going on.
-  sharedService.peer1 = new Peer();
-  // Create another Peer with our demo API key to connect to.
-  sharedService.peer2 = new Peer({
-    key: 'lwjd5qra8257b9',
-    debug: 3
-  });
+//   // Create a new Peer with our demo API key, with debug set to true so we can
+//   // see what's going on.
+//   sharedService.peer1 = new Peer();
+//   // Create another Peer with our demo API key to connect to.
+//   sharedService.peer2 = new Peer({
+//     key: 'lwjd5qra8257b9',
+//     debug: 3
+//   });
 
-  // The `open` event signifies that the Peer is ready to connect with other
-  // Peers and, if we didn't provide the Peer with an ID, that an ID has been
-  // assigned by the server.
-  sharedService.peer1.on('open', function(id) {
-    sharedService.peerId1 = id;
+//   // The `open` event signifies that the Peer is ready to connect with other
+//   // Peers and, if we didn't provide the Peer with an ID, that an ID has been
+//   // assigned by the server.
+//   sharedService.peer1.on('open', function(id) {
+//     sharedService.peerId1 = id;
 
-    var c = sharedService.peer2.connect(sharedService.peerId1);
-    c.on('data', function(data) {
-      // When we receive 'Hello', send ' world'.
-      console.log(data);
-      c.send(' peer');
-    });
-  });
+//     var c = sharedService.peer2.connect(sharedService.peerId1);
+//     c.on('data', function(data) {
+//       // When we receive 'Hello', send ' world'.
+//       console.log(data);
+//       c.send(' peer');
+//     });
+//   });
 
-  // Wait for a connection from the second peer.
-  sharedService.peer1.on('connection', function(connection) {
-    // This `connection` is a DataConnection object with which we can send
-    // data.
-    // The `open` event firing means that the connection is now ready to
-    // transmit data.
-    connection.on('open', function() {
-      // Send 'Hello' on the connection.
-      connection.send('Hello,');
-    });
-    // The `data` event is fired when data is received on the connection.
-    connection.on('data', function(data) {
-      // Append the data to body.
-      console.log(data);
-    });
-  });
+//   // Wait for a connection from the second peer.
+//   sharedService.peer1.on('connection', function(connection) {
+//     // This `connection` is a DataConnection object with which we can send
+//     // data.
+//     // The `open` event firing means that the connection is now ready to
+//     // transmit data.
+//     connection.on('open', function() {
+//       // Send 'Hello' on the connection.
+//       connection.send('Hello,');
+//     });
+//     // The `data` event is fired when data is received on the connection.
+//     connection.on('data', function(data) {
+//       // Append the data to body.
+//       console.log(data);
+//     });
+//   });
 
-  return sharedService;
-});
-angular.module("mean.system").controller("ControllerController", ["$scope", "Global", "TendoService",
-  function($scope, Global, TendoService) {
-    var peerId = peerId;
+//   return sharedService;
+// });
+angular.module("mean.system").controller("ControllerController", ["$scope", "Global",
+  function($scope, Global) {
+    var peerId = makeNewPeerId();
+    console.log("peerId: " + peerId);
     var peer = new Peer(peerId, {
       host: window.location.hostname,
       port: 3333,
@@ -71,7 +72,7 @@ angular.module("mean.system").controller("ControllerController", ["$scope", "Glo
       type: "POST",
       success: function(response, textStatus, jqXHR) {
         console.log(response);
-        var playerNumber = response.controllerIndex;
+        var playerNumber = response.controllerIndex + 1;
         console.log("PLAYER NUMBER OF THIS CONTROLLER IS: " + playerNumber);
         var consolePeerId = response.consolePeerId;
         console.log("CONSOLE PEER ID OF THIS GAME SESSION IS: " + consolePeerId);
@@ -86,40 +87,25 @@ angular.module("mean.system").controller("ControllerController", ["$scope", "Glo
     });
 
     var recenterController = function() {
-      $("body").css("paddingTop", (function() {
-        console.log($("window").height())
-        return ($(window).height() - 240) / 2.0;
-      })());
+      $("body").css("paddingTop", ($(window).height() - 240) / 2.0);
     };
     $(window).resize(recenterController);
     $(recenterController);
 
-    //  on connect
-    var c = TendoService.peer2.connect(TendoService.peerId1);
-
-    //    connect
-    //      enable click listeners for controller buttons
-    //        click listeners call connection.send()
-
-    c.on('data', function(data) {
-      // When we receive 'Hello', send ' world'.
-      console.log(data);
-      c.send(' peer');
-    });
+    var signal = function(buttonId, value) {
+      var payload = {
+        buttonId: buttonId,
+        value: value
+      };
+      console.log("Sending this " + JSON.parse(payload));
+    };
 
     $scope.clickButton = function(button) {
-      send_to_console(button, 1);
-    }
+      signal(button, 1);
+    };
 
     $scope.releaseButton = function(button) {
-      send_to_console(button, 0);
-    }
-
-    var send_to_console = function(button, state) {
-      var data = [];
-      data[button] = state;
-      console.log(data);
-    }
-
+      signal(button, 0);
+    };
   }
 ]);
