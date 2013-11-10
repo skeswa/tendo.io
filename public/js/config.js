3e9d08bafd42132dda74a75b7d3de47719425abd
yasabere <yasabere@gmail.com>
2013-11-09 23:25:10 -0500

    fix

--- public/js/config.js
+++ public/js/config.js
@@ -2,21 +2,13 @@
 window.app.config(['$routeProvider',
     function($routeProvider) {
         $routeProvider.
-        when('/articles', {
-            templateUrl: 'views/articles/list.html'
-        }).
-        when('/articles/create', {
-            templateUrl: 'views/articles/create.html'
-        }).
-        when('/articles/:articleId/edit', {
-            templateUrl: 'views/articles/edit.html'
-        }).
-        when('/articles/:articleId', {
-            templateUrl: 'views/articles/view.html'
-        }).
         when('/', {
             templateUrl: 'views/console.html',
             controller: "ConsoleController"
+        }).
+        when('/login', {
+            templateUrl: 'views/login.html',
+            controller: "loginController"
         }).
         otherwise({
             redirectTo: '/'
